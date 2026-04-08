ba568e0bed09d3f7b5b40e3e78ed8ca211e4f906
roboViola <rachtobey@gmail.com>
2026-04-08 11:49:27 -0400

    Set Up Code Structure Via Comments

---
+++ project/project.ino
@@ -0,0 +1,23 @@
+// Libraries
+
+// Declare structures
+
+// Initialize sensors
+
+// Strain gauge sensor function
+
+// Piezoelectric sensor function
+
+// Control motor function
+
+// Haptic feedback function
+
+// setup(): runs once at the beginning of the program to setup and initialize code
+void setup() {
+
+}
+
+// loop(): runs continuously to execute main program functions
+void loop() {
+
+}
