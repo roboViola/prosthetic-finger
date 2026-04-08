de9f90245e6b4be3d9f4c77a9db600fd4c818023
Ibrahim Gusau <igusau160@gmail.com>
2026-04-08 13:45:25 -0400

    Ibrahim Funtion Declarations

--- project/project.ino
+++ project/project.ino
@@ -4,12 +4,20 @@
 
 // Initialize sensors
 
+// EMG sensor function
+void EMGFunction() {
+
+}
 // Strain gauge sensor function
 
 // Piezoelectric sensor function
+void PiezoSensor() {
 
+}
 // Control motor function
+void controlMotor() {
 
+}
 // Haptic feedback function
 
 // setup(): runs once at the beginning of the program to setup and initialize code
