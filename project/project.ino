bc0d370ffb366141659a61dd340e147b94fc2670
Ibrahim Gusau <igusau160@gmail.com>
2026-04-08 14:11:36 -0400

    Control motor and Piezoelectric sensor initial functions made

--- project/project.ino
+++ project/project.ino
@@ -1,6 +1,9 @@
 // Libraries
 
 // Declare structures
+#define STEP_PIN 3
+#define DIR_PIN 4
+#define HAPTIC_PIN 5
 
 // Initialize sensors
 
@@ -11,12 +14,25 @@ void EMGFunction() {
 // Strain gauge sensor function
 
 // Piezoelectric sensor function
-void PiezoSensor() {
+int piezoSensor() {
+  int piezoValue = analogRead(A1);
 
+  int output = map(piezoValue, 0, 1023, 0, 255);
+
+  analogWrite(HAPTIC_PIN, output);
+
+  return output;
 }
 // Control motor function
-void controlMotor() {
+void controlMotor(int emgValue, bool direction) {
+  digitalWrite(DIR_PIN, direction);
 
+  int speedDelay = map(emgValue, 0, 1023, 2000, 200);
+
+  digitalWrite(STEP_PIN, HIGH);
+  delayMicroseconds(speedDelay);
+  digitalWrite(STEP_PIN, LOW);
+  delayMicroseconds(speedDelay);
 }
 // Haptic feedback function
 
